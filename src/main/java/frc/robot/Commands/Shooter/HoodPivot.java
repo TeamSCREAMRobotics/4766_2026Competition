5f70d99a80306a4b82899b7cb80008c707f9ec2e
Dean12-R12 <dinodeanjr1@outlook.com>
2026-02-18 16:31:23 -0600

    Change commands folder to lowercase c

--- src/main/java/frc/robot/Commands/Shooter/HoodPivot.java
+++ src/main/java/frc/robot/Commands/Shooter/HoodPivot.java
@@ -1,4 +1,4 @@
-package frc.robot.Commands.Shooter;
+package frc.robot.commands.Shooter;
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
