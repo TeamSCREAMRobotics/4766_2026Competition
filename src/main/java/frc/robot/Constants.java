e5af7523fa8e17b778f1057be5f94d5ea039150a
Riley Blacklock <rileyzblacklock@gmail.com>
2026-02-22 16:32:48 -0600

    added reset intake pivot command

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,15 +23,15 @@ public class Constants {
   public class IntakeConstants {
     public static final int intakeMotorID = 14;
     public static final int intakePivotID = 12;
-    public static final int kP = 0;
-    public static final int kI = 0;
-    public static final int kD = 0;
-    public static final int kV = 0;
-    public static final int kG = 0;
+    public static final double kP = 1;
+    public static final double kI = 0;
+    public static final double kD = 0;
+    public static final double kV = 0;
+    public static final double kG = 0;
     public static final double intakeMagicAcceleration = 0;
     public static final double intakeMagicVelocity = 0;
-    public static final double intakePivotDownSetpoint = 0;
-    public static final double intakePivotUpSetpoint = 0;
+    public static final double intakePivotDownSetpoint = 0.05;
+    public static final double intakePivotUpSetpoint = -7.4;
     public static final double intakeAgitateSetpoint = 0;
   }
 
@@ -89,11 +89,11 @@ public class Constants {
 
   public class ClimberConstants {
     public static final int climbermotorID = 20;
-    public static final int kP = 0;
-    public static final int kI = 0;
-    public static final int kD = 0;
-    public static final int kV = 0;
-    public static final int kG = 0;
+    public static final double kP = 0;
+    public static final double kI = 0;
+    public static final double kD = 0;
+    public static final double kV = 0;
+    public static final double kG = 0;
     public static final int climberTopSetpoint = 0;
     public static final int climberLowSetpoint = 0;
   }
