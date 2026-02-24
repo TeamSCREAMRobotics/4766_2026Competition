c8bac5fb46a8049c911cb88016d6a31f02e01c0a
Landon <landonallee@gmail.com>
2026-02-24 16:06:57 -0600

    formatting issues + changes for testing

--- src/main/java/frc/robot/Commands/Shooter/ShootLeft.java
+++ src/main/java/frc/robot/Commands/Shooter/ShootLeft.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Commands.Shooter;
+package frc.robot.commands.Shooter;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ShooterSubFolder.ShooterSub;
