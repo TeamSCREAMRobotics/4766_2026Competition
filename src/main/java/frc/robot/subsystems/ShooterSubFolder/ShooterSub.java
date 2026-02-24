2e505777278113aec7e9204ca35f7b75addb4979
TeamSCREAMBot <teamscreamprogramming@gmail.com>
2026-02-24 20:40:00 +0000

    Apply Spotless formatting

--- src/main/java/frc/robot/subsystems/ShooterSubFolder/ShooterSub.java
+++ src/main/java/frc/robot/subsystems/ShooterSubFolder/ShooterSub.java
@@ -16,7 +16,6 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Constants.ShooterConstants;
 
 public class ShooterSub extends SubsystemBase {
   TalonFX LshooterMotor = new TalonFX(Constants.ShooterConstants.LshooterMoterID);
