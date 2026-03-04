1dbd0ccf90e90cc18a1d0250d2bff4a4fe074548
TeamSCREAMBot <teamscreamprogramming@gmail.com>
2026-03-04 00:31:59 +0000

    Apply Spotless formatting

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,8 +48,7 @@ public class Constants {
     public static final double intakePivotUpSetpoint = 0.1;
     public static final double intakeClimbSetpoint = 0.1;
     public static final double intakeAgitateHighSetpoint = 6.5;
-    public static final double intakeAgitateLowSetpoint = 8.0
-    ;
+    public static final double intakeAgitateLowSetpoint = 8.0;
     public static final double intakePivotForwardSoftLimit = 12.5;
     public static final double intakePivotReverseSoftLimit = -0.3;
   }
