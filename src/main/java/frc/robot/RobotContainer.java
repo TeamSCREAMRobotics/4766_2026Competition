a908f732435725e05163246e03aa44cd04087987
TeamSCREAMBot <teamscreamprogramming@gmail.com>
2026-03-10 21:01:54 +0000

    Apply Spotless formatting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -131,11 +131,7 @@ public class RobotContainer {
                             -driverController.getLeftY()
                                 * MaxSpeed
                                 / 0.6) // Drive forward with negative Y (forward)
-                        .withVelocityY(
-                            -driverController.getLeftX()
-                                * MaxSpeed
-                                / 0.6)
-                ));
+                        .withVelocityY(-driverController.getLeftX() * MaxSpeed / 0.6)));
     // Note that X is defined as forward according to WPILib convention,
     // and Y is defined as to the left according to WPILib convention.
     drivetrain.setDefaultCommand(
