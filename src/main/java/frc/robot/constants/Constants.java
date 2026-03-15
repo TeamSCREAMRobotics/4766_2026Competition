6fe049e00d6e1ecdeffd27b394d7a7f29035fe8a
clairesnell <claire.e.snell@gmail.com>
2026-03-14 20:47:40 -0500

    Update new indexer

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -42,12 +42,12 @@ public class Constants {
     public static final double kS = 1;
     public static final double intakeMagicAcceleration = 52;
     public static final double intakeMagicVelocity = 60;
-    public static final double intakePivotDownSetpoint = 11.8;
+    public static final double intakePivotDownSetpoint = 7.5;
     public static final double intakePivotUpSetpoint = 0.1;
     public static final double intakeClimbSetpoint = 0.1;
-    public static final double intakeAgitateHighSetpoint = 6.5;
-    public static final double intakeAgitateLowSetpoint = 8.0;
-    public static final double intakePivotForwardSoftLimit = 12.5;
+    public static final double intakeAgitateHighSetpoint = 5.5;
+    public static final double intakeAgitateLowSetpoint = 6.5;
+    public static final double intakePivotForwardSoftLimit = 7.8;
     public static final double intakePivotReverseSoftLimit = -0.3;
   }
 
@@ -56,6 +56,7 @@ public class Constants {
     public static final int RshooterMotorID = 16;
     public static final int shooterCANID = 0;
 
+    
     public static final int pivotMotorID = 0;
     public static final int pivotCanID = 0;
 
