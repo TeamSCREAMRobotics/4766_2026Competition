9d9a931c5a741a936944cfca364917a6ad4f4f22
Landon <landonallee@gmail.com>
2026-03-24 19:52:14 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -381,7 +381,7 @@ public class RobotContainer {
                 .alongWith(new Shoot(m_flywheel, m_indexer, getDesiredShooterVelocity))
                 .alongWith(new Jostle(m_intake)));
 
-    m_indexer.setDefaultCommand(Commands.run(() -> m_indexer.runIndexer(0), m_indexer));
+    m_indexer.setDefaultCommand(Commands.run(() -> m_indexer.runIndexer(-2), m_indexer));
     m_flywheel.setDefaultCommand(
         Commands.run(
             () -> m_flywheel.setSetpointVelocity(getDesiredShooterVelocity.getAsDouble() * 0.25),
